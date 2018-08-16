6d11e1e0f51dc7a613f5d3acdf4615976104755c
TouchInTouch <35906277+IMGameDesigner@users.noreply.github.com>
2018-08-16 12:19:04 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -13,105 +13,15 @@ cc.Class({
 		SP_in_2: {default: null,type: cc.Node},
 		SP_in_3: {default: null,type: cc.Node},
 		//MainScene:
-		history_W:200,
-		history_H:130,
-		history_midX:640,
-		history_midY:900,
-	    button_circle_white_X:{
-            default: function () { 
-                return [1920*3/12,1920*5/12,1920*7/12,1920*9/12];
-            },
-            type: [cc.Integer]
-        },
-		button_circle_white_Y:{
-            default: function () { 
-                return [1080/2,1080/2,1080/2,1080/2];
-            },
-            type: [cc.Integer]
-        },
-		button_money:{
-            default: function () { 
-                return [100,1000,10000,100000,1000000];
-            },
-            type: [cc.Integer]
-        },
-		button_money_X:{
-            default: function () { 
-                return [1920*4/12,1920*5/12,1920*6/12,1920*7/12,1920*8/12];
-            },
-            type: [cc.Integer]
-        },
-		button_money_Y:{
-            default: function () { 
-                return [1080/12,1080/12,1080/12,1080/12,1080/12];
-            },
-            type: [cc.Integer]
-        },
-		paiX:{
-            default: function () { 
-                return [860,910,960,1010,1060,
-                        380,430,480,530,580,
-                        700,750,800,850,900,
-	                    1020,1070,1120,1170,1220,
-	                    1340,1390,1440,1490,1540];
-            },
-            type: [cc.Integer]
-        },
-		paiY:{
-            default: function () { 
-                return [360,360,360,360,360,
-                        720,720,720,720,720,
-                        720,720,720,720,720,
-	                    720,720,720,720,720,
-	                    720,720,720,720,720];
-            },
-            type: [cc.Integer]
-        },
-		headX:{
-            default: function () { 
-                return [1920/2,1920/8,1920*3/8,1920*5/8,1920*7/8];
-            },
-            type: [cc.Integer]
-        },
-		headY:{
-            default: function () { 
-                return [1080*1/5,1080*10/12,1080*10/12,1080*10/12,1080*10/12];
-            },
-            type: [cc.Integer]
-        },
-		ox_bei:{
-            default: function () { 
-                return [1//1 ox1
-	                   ,1//2 ox2
-	                   ,1//3 ox3
-	                   ,1//4 ox4
-	                   ,1//5 ox5
-	                   ,1//6 ox6
-	                   ,1//7 ox7
-	                   ,2//8 ox8
-	                   ,3//9 ox9
-	                   ,4//10 oxox
-	                   ,5//11 炸弹
-	                   ,6//12 五花
-	                   ,10]//13 五小];
-            },
-            type: [cc.Integer]
-        },
+		caser:0,
 		casert:0,
-        my_gived_money:{
-            default: function () { 
-                return [0,0,0,0];
-            },
-            type: [cc.Integer]
-        },
-        my_money_tip:1,
 		room:0,
 		sofa:0,
-		timer0:0,
-		BY_history:"FFFFFFFFFFFFFFFFFFFFFFFF",
+		timer0:0,//时间系统随时更新的本地动画
+		/*
 		BY_cardssize5X5:"11111aaaaaddddd5555599999",
 		BY_cardscolor5X5:"0000011111222223333300000",
-		BY_cardscolor5:"11111",
+		BY_cardssize5:"11111",
 		BY_cardscolor5:"11111",
 		BY_link:{
             default: function () { 
@@ -129,8 +39,6 @@ cc.Class({
 		BY_name:[],
 		BY_id:[],
 		BY_photo:[],
-		BY_allgivedmoney4:[],
-		BY_pairank:[],
 		BY_qiang:{
             default: function () { 
                 return [0,0,0,0,0];
@@ -138,44 +46,39 @@ cc.Class({
             type: [cc.Integer]
         },
 		BY_zhuang:0,
-		money_mygive:{
-            default: function () { 
-                return [0,0,0,0];//东南西北
-            },
-            type: [cc.Integer]
-        },
 		//{动画变量：
 		pai_action_ed:true,
 		paiback_action_ed:true,
 		oxback_action_ed:true,
 		chengback_action_ed:true,
+		*/
 		//【SP数据】:
-		SP_head_:[],//={o1,o2,o3,o4,o5,o6,o7,o8,o9,o10,o11,o12}
-		SP_head:{default:[],type: [cc.Node]},
-		SP_pai:{default:[],type: [cc.Node]},
-		SP_ox:{default:[],type: [cc.Node]},
-		SP_cheng:{default:[],type: [cc.Node]},
-		SP_qiang:{default:[],type: [cc.Node]},
-		SP_ready:{default:[],type: [cc.Node]},
-		SP_without:{default:[],type: [cc.Node]},
-		ZI_name:{default:[],type: [cc.Label]},
-		ZI_personmoney:{default:[],type: [cc.Label]},
-		SP_money:{default:[],type: [cc.Node]},//动画
-		SP_button_money:[],//={o1,o2,o3,o4,o5}
-		SP_button_circle_white:[],//={o1,o2,o3,o4}
-		ZI_givemoney:[],//={o1,o2,o3,o4}
-		ZI_allgivemoney:[],//={o1,o2,o3,o4}
-		//SP_history={} for i=1,6,1 do SP_history[i]={o1,o2,o3,o4} end
-		ZI_history:[],//={o1,o2,o3,o4}
-		ZI_history_rate:[],//={o1,o2,o3,o4}
 		SP_BG: {default: null,type: cc.Node},
 		SP_exit: {default: null,type: cc.Node},
-		SP_button_ready: {default: null,type: cc.Node},
-		SP_button_qiang: {default: null,type: cc.Node},
-		SP_zhuang: {default: null,type: cc.Node},
-		ZI_caser: {default: null,type: cc.Label},
-		ZI_casert: {default: null,type: cc.Label},
 		SP_note: {default: null,type: cc.Node},
+		SP_button_ready: {default: null,type: cc.Node},
+		ZI_room: {default: null,type: cc.Label},
+		SP_clock: {default: null,type: cc.Node},
+		ZI_casert: {default: null,type: cc.Label},
+		SP_head:{default:[],type: [cc.Node]},
+		ZI_name:{default:[],type: [cc.Label]},
+		ZI_personmoney:{default:[],type: [cc.Label]},
+		SP_ready:{default:[],type: [cc.Node]},
+		SP_mycard:{default:[],type: [cc.Node]},
+		ZI_other2_pai_sum: {default: null,type: cc.Label},
+		ZI_other3_pai_sum: {default: null,type: cc.Label},
+		SP_landowner_card:{default:[],type: [cc.Node]},
+		SP_qiang:{default:[],type: [cc.Node]},
+		SP_button_qiang: {default: null,type: cc.Node},
+		SP_without:{default:[],type: [cc.Node]},
+		SP_out_card:{default:[],type: [cc.Node]},
+		ZI_getmoney:{default:[],type: [cc.Label]},
+		SP_zhuang: {default: null,type: cc.Node},
+		SP_button_tishi: {default: null,type: cc.Node},
+		SP_button_chupai: {default: null,type: cc.Node},
+		SP_button_buyao: {default: null,type: cc.Node},
+		//SP_money:{default:[],type: [cc.Node]},//动画
+		//----------------------------------------
 		websocket:[],
     },
 
@@ -194,7 +97,7 @@ cc.Class({
 		//-------------------------------------------------------------------------------------
 		//-------------------------------------------------------------------------------------
 		var self=this;
-		this.SP_exit.on(cc.Node.EventType.TOUCH_START, function(event){ 
+		//this.SP_exit.on(cc.Node.EventType.TOUCH_START, function(event){ 
 		    //断开连接
 			/*
 			socket:close()
@@ -202,15 +105,15 @@ cc.Class({
 			local newScene = import("app.scenes.HomeScene"):new()
             display.replaceScene(newScene,"crossFade",0,0)
 			*/
-		}, this.node);
+		//}, this.node);
 
-		this.SP_button_ready.on(cc.Node.EventType.TOUCH_START, function(event){ 
+		/*this.SP_button_ready.on(cc.Node.EventType.TOUCH_START, function(event){ 
 		    self.websocket[0].send("R}");
 		}, this.node);	    
 
 		this.SP_button_qiang.on(cc.Node.EventType.TOUCH_START, function(event){ 
 		    self.websocket[0].send("Q}");
-		}, this.node);	
+		}, this.node);	*/
 		
 		this.SP_in_1.on(cc.Node.EventType.TOUCH_START, function(event){ 
 		    self.SP_in_1.active=false;
